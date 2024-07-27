bfa8c279b15e11430f0b71bf7b6de3e42451f844
ugbeadie <ugbeadie3@gmail.com>
2024-07-27 17:26:09 +0100

    sign in with google and logout

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,14 +1,36 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  //   console.log(auth?.currentUser?.email);
+
   const signIn = async () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
     } catch (err) {
       console.error(err);
     }
@@ -26,6 +48,8 @@ const Auth = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <button onClick={signIn}>sign in</button>
+      <button onClick={signInWithGoogle}>sign in with gogle</button>
+      <button onClick={logout}>log out</button>
     </div>
   );
 };
