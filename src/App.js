142ebfa88e2fe19fe6d549a802e49f44cc884802
ugbeadie <ugbeadie3@gmail.com>
2024-07-28 16:22:14 +0100

    begin adding data to firestore

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Auth from "./components/Auth";
 import { db } from "./config/firebase";
 import { getDocs, collection } from "firebase/firestore";
+import "./App.css";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
@@ -11,7 +12,11 @@ function App() {
     const getMovieList = async () => {
       try {
         const data = await getDocs(moviesCollectionRef);
-        console.log(data);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMovieList(filteredData);
       } catch (err) {
         console.error(err);
       }
@@ -22,6 +27,21 @@ function App() {
   return (
     <>
       <Auth />
+      <div className="container">
+        <input type="text" placeholder="title" />
+        <input type="number" placeholder="release date" />
+        <input type="checkbox" />
+        <label>received an oscar</label>
+        <button>submit movie</button>
+      </div>
+      {movieList.map((movie) => (
+        <div className="container">
+          <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+            {movie.title}
+          </h1>
+          <p>Date: {movie.releaseDate}</p>
+        </div>
+      ))}
     </>
   );
 }
