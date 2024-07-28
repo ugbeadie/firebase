06df839282059682685e0d09821321be4303ea66
ugbeadie <ugbeadie3@gmail.com>
2024-07-28 15:32:35 +0100

    getDocs from firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import Auth from "./components/Auth";
+import { db } from "./config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const moviesCollectionRef = collection(db, "movies");
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getMovieList();
+  }, []);
+
   return (
     <>
       <Auth />
